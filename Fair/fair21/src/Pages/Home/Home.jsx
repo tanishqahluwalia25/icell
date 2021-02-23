5e05dbace49ea5e0cda522d7ed72b655579db006
tanishqahluwalia25 <tanishqwalia25@gmail.com>
2021-02-23 21:25:15 +0530

    sass not added

---
+++ Fair/fair21/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+// import "./style.scss";
+import {logo} from "../../assets/logo.png"
+export default function Home() {
+
+    return (
+        <div>
+            <header>
+                <img src={logo} alt=""/>
+            </header>
+        </div>
+    );
+}
