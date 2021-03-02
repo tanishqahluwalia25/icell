6d9f5cb0d56a85d9038b6cc72ecd9e9c1f100ca8
tanishqahluwalia25 <tanishqwalia25@gmail.com>
2021-03-02 09:17:33 +0530

    added first page ui grey

--- Fair/fair21/src/App.js
+++ Fair/fair21/src/App.js
@@ -4,6 +4,8 @@ import {Switch, BrowserRouter, Route} from 'react-router-dom'
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
   import 'react-toastify/dist/ReactToastify.css';
+import UpdateDetails from './Pages/UpdateDetails/UpdateDetails';
+import Profile from './Pages/Profile/Profile';
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
           <Route exact path="/home" component={Home}/>
           <Route exact path="/login" component={Login}/>
           <Route exact path="/register" component={Register}/>
+          <Route exact path="/new-user" component={UpdateDetails}/>
+          <Route exact path="/profile" component={Profile}/>
           <Route path="/" component={Home}/>
           </Switch>
         </BrowserRouter>
--- Fair/fair21/src/Pages/Home/Home.jsx
+++ Fair/fair21/src/Pages/Home/Home.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import hrc from "../../assets/poster.png";
-import { Navbar, Nav, Row, Col } from "react-bootstrap"
+import { Navbar, Nav, Row, Col, Form } from "react-bootstrap"
 
 import './style.scss'
 import { Button } from "react-bootstrap";
 import ReactRotatingText from 'react-rotating-text'
 import { NavLink } from 'react-router-dom';
 import firebase from '../../config'
+import { toast, ToastContainer } from 'react-toastify';
 
 export default function Home() {
-
+    const [query, setQuery] = useState(' ')
     const [user, setUser] = useState('');
     useEffect(() => (
         setUser({ email: localStorage.getItem("email"), uid: localStorage.getItem("uid") })
@@ -21,49 +22,104 @@ export default function Home() {
         localStorage.removeItem("email");
         setUser('')
     }
+
+    const handleChange = e => {
+        setQuery(e.target.value)
+    }
+    const navbarRef = useRef();
+    const navbarStyleListener = () => {
+        if (window.location.pathname === '/' && window.scrollY < 90) {
+            navbarRef.current.id = '';
+        } else {
+            navbarRef.current.id = 'shadow';
+        }
+    };
+    useEffect(() => { window.addEventListener('scroll', navbarStyleListener); }, [])
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        firebase.database().ref('queries/' + localStorage.getItem('uid') + '/' + new Date()).set({ query: query, email: localStorage.getItem('email') });
+        toast("Stay Tuned with us on instagram to get answers to your query!")
+        setQuery('')
+    }
     return (
-        <header className='home'>
-            <Navbar className='px-5' bg='dark' variant="dark" expand='lg'>
-                <Navbar.Brand href="#home">Internship Cell</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">About</Nav.Link>
 
-                    </Nav>
-                    {
-                        user?.uid === null ? (<Nav>
-                            <NavLink to='/register' className='mx-1 my-1 btn-outline-light btn' variant="outline-light" >Register</NavLink>
-                            <NavLink to='/login' className='mx-1 my-1 btn-outline-light btn' variant="outline-light" >Login</NavLink>
+        <div>
+            <header className='home bg-dark'>
+                <Navbar ref={navbarRef} fixed='top' className='px-5' bg='dark' variant="dark" expand='lg'>
+                    <Navbar.Brand href="#home">Internship Cell</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/home" >Home</Nav.Link>
+                            <Nav.Link href="/home" >Companies</Nav.Link>
+                            {/* <Nav.Link href="/home" >Home</Nav.Link> */}
+                            <Nav.Link target='_blank' href="https://www.placementhansraj.com/internships.html">About</Nav.Link>
 
-                        </Nav>) : (
-                                <Nav>
-                                    <Button onClick={handleLogout} className='mx-1 my-1' variant="outline-light" >Logout</Button>
-                                    <Button className='mx-1 my-1' variant="outline-light" >My Profile</Button>
-                                </Nav>
-                            )
-                    }
+                        </Nav>
+                        {
+                            user?.uid === null ? (<Nav>
+                                <NavLink to='/register' className='mx-1 my-1 btn-outline-light btn' variant="outline-light" >Register</NavLink>
+                                <NavLink to='/login' className='mx-1 my-1 btn-outline-light btn' variant="outline-light" >Login</NavLink>
 
-                </Navbar.Collapse>
-            </Navbar>
+                            </Nav>) : (
+                                    <Nav>
+                                        <Button onClick={handleLogout} className='mx-1 my-1' variant="outline-light" >Logout</Button>
+                                        <NavLink to='profile' className='btn-outline-light btn mx-1 my-1' variant="outline-light" >My Profile</NavLink>
+                                    </Nav>
+                                )
+                        }
 
-            <Row className='row1 center'>
-                <Col md="4" sm="10" className="center">
-                    <img src={hrc} alt="" />
-                </Col>
-
-                <Col md="6" sm="10" className="center justify-content-center ">
-                    <div className="rot center">
-                        <div className="line mx-5 " ></div>
-                        <div className='w-100'>
-                            <p className="rot-text ">Are you looking for</p>
-                            <ReactRotatingText className='rot-text text-danger' items={['Opportunties?', 'Exposure?', 'Internships?']} />
-                        </div></div>
-                </Col>
-            </Row>
+                    </Navbar.Collapse>
+                </Navbar>
 
 
-        </header>
+                <Row className='h-100 ' id='text1'>
+                    <Col md='1' sm='0'>
+
+                    </Col>
+                    <Col md={10} sm={12} className='w-75 w-sm-100 ml-sm-5 my-auto'>
+                        <Row>
+                            <Col>
+                                <p className="rot-text ">Are you looking for</p>
+                            </Col>
+                        </Row>
+                        <Row><Col><ReactRotatingText style={{ color: "white" }} className='rot rot-text ' id='' items={['#Opportunties?', '#Exposure?', '#Internships?']} /></Col>
+                        </Row>
+                        <Row>
+
+
+
+                            {
+                                localStorage.getItem('uid') ?
+                                    (<Col md={10}>
+                                        <input className='bg-dark mr-2 py-1 pr-5 pl-1 rounded' style={{
+                                            color: "grey"
+                                        }} type="text" id='name' onChange={handleChange} />
+                                        <button className='mx-0 my-5 btn' id='rot' onClick={handleSubmit}>Submit</button>
+                                    </Col>)
+                                    : (
+                                        <Col md={10}><button className='mx-0 my-5 btn' id='rot'>Register Now</button> </Col>
+                                    )
+                            }
+
+                        </Row>
+                        <Row>
+                            <Col md='1' sm='3'><i className="icon fa fa-2x fa-instagram w-25" aria-hidden="true"></i></Col>
+                            <Col md='1' sm='3'><i className="icon fa fa-2x fa-linkedin w-25" aria-hidden="true"></i></Col>
+                            <Col md='1' sm='3'><i className="icon fa fa-2x fa-envelope-o w-sm-25" aria-hidden="true"></i></Col>
+                        </Row>
+                    </Col>
+
+                </Row>
+
+                <ToastContainer />
+            </header>
+
+            <section style={{ height: '100vh' }} >
+                hcfdsujkafdwsa
+                fedwsfedwsa
+            </section>
+        </div>
     );
 }
